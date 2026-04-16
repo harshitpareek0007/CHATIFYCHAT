350876658c2e406fc0ec5431dc6cea10115078d9
harshitpareek0007 <harshitpareek0007@gmail.com>
2026-04-16 20:59:14 +0530

    removed mongo log

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 async function connectDB(){
-   console.log('MONGO_URI:', process.env.MONGO_URI);
+
    await mongoose.connect(process.env.MONGO_URI)
    console.log(`database connected`);
    
