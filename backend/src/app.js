d3fca06d5a46f9d80b1a6c8c4cdce7e1271d9375
harshitpareek0007 <harshitpareek0007@gmail.com>
2026-04-16 21:13:53 +0530

    fixed cors for deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,6 @@
+require('dotenv').config()
 const express = require('express');
+
 const cookieParser = require('cookie-parser')
 
 const cors = require('cors');
@@ -7,12 +9,10 @@ const cors = require('cors');
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors(
-    {
-        credentials:true,
-        origin:"http://localhost:5173"
-    }
-))
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL
+}));
 
 
 const authRouter = require('./routes/auth.route')
